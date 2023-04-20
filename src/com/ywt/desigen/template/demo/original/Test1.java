4474f1d72031d6145ef2634b7fa34066eb4d4e4d
YuanWuT <676482445@qq.com>
2023-04-20 15:46:09 +0800

    template 模板方法增加测试类

---
+++ src/com/ywt/desigen/template/demo/original/Test1.java
@@ -0,0 +1,11 @@
+package com.ywt.desigen.template.demo.original;
+
+public class Test1 {
+    public static void main(String[] args) {
+        MiddleStusent ms = new MiddleStusent();
+        ms.setName("中学生阿中");
+        ms.write();   MiddleStusent cs = new MiddleStusent();
+        cs.setName("小学生阿小");
+        cs.write();
+    }
+}
